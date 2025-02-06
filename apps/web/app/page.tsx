9d3d5cc61c9eefcc308a49f2c8875e9f9c5cfbb4
youking-lib <whistleyz@163.com>
2025-02-06 18:04:17 +0800

    feat(web): implement file upload functionality in omnibox component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,81 +1,27 @@
 'use client';
 
-import { useRef } from 'react';
-import { useImmer } from 'use-immer';
-import * as motion from 'motion/react-client';
-import { PlusIcon } from '@radix-ui/react-icons';
-import { Box, Button, Flex } from '@radix-ui/themes';
+import { Box, Container, Flex, Section } from '@radix-ui/themes';
 
-import { Input } from '@/components/ui/input';
-import { RetrieveOTP } from '@/components/retrieve-otp';
-
-import { Filelist } from './filelist';
 import { Header } from './header';
 import { Ominibox } from '@/components/ominibox';
 
 export default function Home() {
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const [state, setState] = useImmer({
-    code: '',
-    files: [] as File[]
-  });
-
   return (
     <Box width="100vw">
       <Header />
 
-      <Flex height="calc(100vh - 84px)" align="center" justify="center">
-        <Ominibox />
-      </Flex>
+      <Container>
+        <Section
+          size="1"
+          height="calc(100vh - 84px)"
+          className="flex justify-center items-center"
+          px="2"
+        >
+          <Ominibox />
+        </Section>
+      </Container>
 
       <Flex height="100vh">123</Flex>
     </Box>
   );
 }
-
-// <motion.div
-// initial={{ opacity: 0, y: -20 }}
-// animate={{ opacity: 1, y: 0 }}
-// transition={{ duration: 0.5, delay: 0.2 }}
-// >
-// <Flex direction="column" gap="8" align="center">
-//   {/* <div className="text-2xl font-bold">赛博取件码，畅享极致的文件传输体验！</div> */}
-
-//   <div className="flex items-center gap-2">
-//     取件码：
-//     <RetrieveOTP
-//       value={state.code}
-//       onComplete={() => {
-//         console.log('complete', state.code);
-//       }}
-//       onChange={value => {
-//         setState(draft => {
-//           draft.code = value;
-//         });
-//       }}
-//     />
-//   </div>
-
-//   <div>
-//     <Button size="3" highContrast color="gray" onClick={() => inputRef.current?.click()}>
-//       Upload File <PlusIcon height="16" width="16" />
-//     </Button>
-
-//     <Input
-//       ref={inputRef}
-//       id="picture"
-//       className="hidden"
-//       type="file"
-//       multiple
-//       onChange={e => {
-//         setState(draft => {
-//           draft.files = Array.from(e.target.files ?? []);
-//         });
-//       }}
-//     />
-//   </div>
-
-//   <Filelist files={state.files} />
-// </Flex>
-// </motion.div>
--- apps/web/components/ominibox.tsx
+++ apps/web/components/ominibox.tsx
@@ -1,20 +1,20 @@
+import { useRef } from 'react';
 import { useImmer } from 'use-immer';
 import { CloudUploadIcon } from 'lucide-react';
-import { Box, Card, Flex, Heading, Inset, Text } from '@radix-ui/themes';
+import { Card, Flex, Heading, Inset, Text } from '@radix-ui/themes';
+
+import { Input } from '@/components/ui/input';
+import { Filelist } from './filelist';
 
 import { RetrieveOTP } from './retrieve-otp';
 
 export function Ominibox() {
   return (
-    <Card className="min-w-[1200px] min-h-[600px] flex justify-between">
+    <Card className="w-full min-h-[600px] flex justify-between">
       <Flex className="w-1/2" py="6" align="center" justify="center" direction="column" gap="4">
         <Retrieve />
       </Flex>
-      <Inset
-        className="w-1/2 bg-zinc-100 flex justify-center items-center flex-col gap-2 cursor-pointer"
-        clip="padding-box"
-        side="right"
-      >
+      <Inset className="w-1/2" clip="padding-box" side="right">
         <Upload />
       </Inset>
     </Card>
@@ -23,8 +23,7 @@ export function Ominibox() {
 
 function Retrieve() {
   const [state, setState] = useImmer({
-    code: '',
-    files: [] as File[]
+    code: ''
   });
 
   return (
@@ -48,11 +47,36 @@ function Retrieve() {
 }
 
 function Upload() {
+  const [state, setState] = useImmer({
+    code: '',
+    files: [] as File[]
+  });
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <>
+    <div
+      className="w-full h-full bg-zinc-100 flex justify-center items-center flex-col gap-2 cursor-pointer"
+      onClick={() => inputRef.current?.click()}
+    >
       <CloudUploadIcon className="text-neutral-400 size-12" strokeWidth={0.5} />
 
       <div className="text-neutral-400">Upload files</div>
-    </>
+
+      <Input
+        ref={inputRef}
+        id="picture"
+        className="hidden"
+        type="file"
+        multiple
+        onChange={e => {
+          setState(draft => {
+            draft.files = Array.from(e.target.files ?? []);
+          });
+        }}
+      />
+
+      {state.files.length > 0 && <Filelist files={state.files} />}
+    </div>
   );
 }
